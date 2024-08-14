be3e0d3def728fc8621d40d3e2e85e5e1aca6de6
oscar.david.moreno <oscar.david.moreno@gmail.com>
2024-08-14 07:58:32 +0000

    Fix CSS styles

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // Add JS here
+
+// midu query 
+const $ = el => document.querySelector(el);
+const $$ = el => document.querySelectorAll(el);
+
+const imageInput = $('#image-imput');
+const itemsSection = $('#selector-items');
+
+imageInput.addEventListener('change', (event) => {
+    const [file] = event.target.files
+
+    if(!file) return;
+    if(file.length == 0) return;
+
+    const reader = new FileReader();
+    reader.onload = (eventReader) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = eventReader.target.result;
+        itemsSection.appendChild(imgElement);
+        imgElement.classList.add('item-image');
+        itemsSection.appendChild(imgElement);
+    }
+
+    reader.readAsDataURL(file);
+});
