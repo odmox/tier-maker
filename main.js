f49c3ee34e3d73cc9aa3a0fb2c5cec54bc89b161
oscar.david.moreno <oscar.david.moreno@gmail.com>
2024-08-14 08:29:25 +0000

    drag midu error

--- main.js
+++ main.js
@@ -15,12 +15,60 @@ imageInput.addEventListener('change', (event) => {
 
     const reader = new FileReader();
     reader.onload = (eventReader) => {
-        const imgElement = document.createElement('img');
-        imgElement.src = eventReader.target.result;
-        itemsSection.appendChild(imgElement);
-        imgElement.classList.add('item-image');
-        itemsSection.appendChild(imgElement);
+        createImage(eventReader.target.result);
     }
 
     reader.readAsDataURL(file);
 });
+
+let draggedElement = null;
+let sourceContainer = null; 
+
+const rows = $$('.tier .row');
+
+rows.forEach(row => {
+    row.addEventListener('drop', handleDrop);
+    row.addEventListener('dragover', handleDragOver);
+    row.addEventListener('dragleave', handleDragLeave);
+});
+
+function handleDrop(event) {
+    event.preventDefault();
+    
+    const { currentTarget, dataTransfer } = event;
+    console.log(currentTarget);
+    console.log(dataTransfer);
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+
+}
+
+function handleDragLeave(event) {
+    event.preventDefault();
+}
+
+function handleDragStart(event) {
+    draggedElement = event.target;
+    sourceContainer = event.target.parentElement;
+}
+
+function handleDragEnd(event) {
+    draggedElement = null;
+    sourceContainer = null;
+}
+
+function createImage(src){
+    const imgElement = document.createElement('img');
+    imgElement.draggable = true;
+    imgElement.src = src;
+    itemsSection.appendChild(imgElement);
+    imgElement.classList.add('item-image');
+    
+    imgElement.addEventListener('dragstart', handleDragStart);
+    imgElement.addEventListener('dragend', handleDragEnd);
+    
+    itemsSection.appendChild(imgElement);
+    return imgElement;
+}
